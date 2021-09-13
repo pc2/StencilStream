d2889c5ba3f753f7bf54cf6dfdde72b60f8299e4
Jan-Oliver "Janonard" Opdenhövel <joo@mail.uni-paderborn.de>
2021-09-13 17:13:41 +0200

    Upgrading to OneAPI 2021.3 and BSP 20.4.0

--- examples/build.sh
+++ examples/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source /cm/shared/opt/intel_oneapi/2021.1.1/setvars.sh
-ml compiler/GCC nalla_pcie/19.4.0
+source /cm/shared/opt/intel_oneapi/2021.3/setvars.sh
+ml compiler/GCC nalla_pcie/20.4.0
 
 make all -j$(nprocs)
