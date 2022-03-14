cf4a0592aa04a00070cb650464ee1e9f0be4d98e
Jan-Oliver "Janonard" Opdenhövel <joo@mail.uni-paderborn.de>
2022-03-14 12:19:17 +0100

    Improving on the build script

--- examples/fdtd/build.sh
+++ examples/fdtd/build.sh
@@ -7,112 +7,131 @@
 # 
 # THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-if [[ $@ == *"-h"* || $@ == *"--help"* || -z $1 ]]
+if [[ $@ == *"-h"* || $@ == *"--help"* || $# < 3 ]]
 then
 cat <<EOF
-Usage: $0 variant
+Usage: $0 material_resolver source backend [modifier]
 
-This script builds variants of the FDTD application. The application was many different ways to do
+This script builds variants of the FDTD application. The application has many different ways to do
 things, which are selected via macro constants and templates throughout the code. The actual
-combinations one might want to build are expressed via the passed executable name. The scheme of 
-the variant names is:
+combinations one might want to build are expressed set using the CLI arguments.
 
-fdtd_<material_resolver>_<source>_<backend>[_<modifier>]
-
-The `material_resolver` denotes how the material of a cell is stored in a cell and how the 
+The 'material_resolver' denotes how the material of a cell is stored in a cell and how the 
 material coefficients are retrieved from it. Possible values are:
-* `coef_mat`: Store the final material coefficients directly in every cell.
-* `lut_mat`: Store a lookup table with all known material coefficients in the kernel and store only an index in the cell.
+* 'coef': Store the final material coefficients directly in every cell.
+* 'lut': Store a lookup table with all known material coefficients in the kernel and store only an index in the cell.
 
-The `source` denotes whether the computations of the source wave amplitude are done by the FPGA or
-the host. If the wave is computed on the host, the amplitudes are simply stored in a look-up table. Possible values are:
-* `oc_src`: Compute the source wave amplitudes on-chip.
-* `lut_src`: Compute the source wave amplitudes on the host and look them up.
+The 'source' denotes whether the computations of the source wave amplitude are done by the FPGA or
+the host. If the wave is computed on the host, the amplitudes are simply stored in a look-up table. 
+Possible values are:
+* 'od': Compute the source wave amplitudes on-demand with the FPGA.
+* 'lut': Compute the source wave amplitudes on the host and store them in a lookup table.
 
 StencilStream offers different backends or executors with different architectures or goals. The possible values are:
-* `mono`: Use the monotile FPGA backend of StencilStream. It yields a higher performance for the 
-  same pipeline length than `tiling`, but it is limited to a maximal grid width and height.
-* `tiling`: Use the tiling FPGA backend of StencilStream. It can handle arbitrarly large grids (and
-  therefore cavity radii and resolutions), but generally yields a lower performance than `mono`.
-* `cpu`: Use the testing CPU backend of StencilStream. This backend is a trivial implementation of
+* 'mono': Use the monotile FPGA backend of StencilStream. It yields a higher performance for the 
+  same pipeline length than 'tiling', but it is limited to a maximal grid width and height.
+* 'tiling': Use the tiling FPGA backend of StencilStream. It can handle arbitrarly large grids (and
+  therefore cavity radii and resolutions), but generally yields a lower performance than 'mono'.
+* 'cpu': Use the testing CPU backend of StencilStream. This backend is a trivial implementation of
   executor interface for CPUs and therefore performs worse than both FPGA backends when synthesized,
   but it's good enough to complete most simulations in reasonable times for testing purposes.
 
 The FPGA backends also support some modifiers:
-* `emu`: Don't synthesize the design and create an emulation image. Note that emulation is *very* 
-  slow and not suitable to test the functionality of the transition function. Use the `cpu` backend 
+* 'emu': Don't synthesize the design and create an emulation image. Note that emulation is *very* 
+  slow and not suitable to test the functionality of the transition function. Use the 'cpu' backend 
   instead.
-* `report`: Generate are hardware usage report for the variant. These reports can deliver a good 
+* 'report': Generate are hardware usage report for the variant. These reports can deliver a good 
   estimate on the synthesized design's performance.
 EOF
-exit 0
+exit 1
 fi
 
-function run_build {
-    EXEC_NAME=$1
-    rm -f $EXEC_NAME
+MATERIAL=$1
+SOURCE=$2
+BACKEND=$3
+MODIFIER=$4
 
-    ARGS="-std=c++20 -DSTENCIL_INDEX_WIDTH=64 -DFDTD_BURST_SIZE=1024 -qactypes -I./ -O3"
+VALID_ARGUMENTS=1
 
-    # Noctua-specific options
-    if [[ -n "$EBROOTGCC" ]]
-    then
-        ARGS="$ARGS --gcc-toolchain=$EBROOTGCC"
-    fi
+ARGS="-std=c++20 -DSTENCIL_INDEX_WIDTH=64 -DFDTD_BURST_SIZE=1024 -qactypes -I./ -O3"
 
-    # Material resolvers
-    if [[ "$EXEC_NAME" == *"coef_mat"* ]]
-    then
-        ARGS="$ARGS -DMATERIAL=0"
-    elif [[ "$EXEC_NAME" == *"lut_mat"* ]]
-    then
-        ARGS="$ARGS -DMATERIAL=1"
-    fi
+# Noctua-specific options
+if [[ -n "$EBROOTGCC" ]]
+then
+    ARGS="$ARGS --gcc-toolchain=$EBROOTGCC"
+fi
 
-    # Source wave computation
-    if [[ "$EXEC_NAME" == *"oc_src"* ]]
-    then
-        ARGS="$ARGS -DSOURCE=0"
-    elif [[ "$EXEC_NAME" == *"lut_src"* ]]
-    then
-        ARGS="$ARGS -DSOURCE=1"
-    fi
+# Material resolvers
+if [[ "$MATERIAL" == "coef" ]]
+then
+    ARGS="$ARGS -DMATERIAL=0"
+elif [[ "$MATERIAL" == "lut" ]]
+then
+    ARGS="$ARGS -DMATERIAL=1"
+else
+    echo "Unknown material resolver '$MATERIAL'." 1>&2
+    VALID_ARGUMENTS=0
+fi
 
-    # FPGA-specific options
-    if [[ "$EXEC_NAME" == *"mono"*  || "$EXEC_NAME" == *"tiling"* ]]
-    then
-        ARGS="$ARGS -fintelfpga -Xsv"
+# Source wave computation
+if [[ "$SOURCE" == "od" ]]
+then
+    ARGS="$ARGS -DSOURCE=0"
+elif [[ "$SOURCE" == "lut" ]]
+then
+    ARGS="$ARGS -DSOURCE=1"
+else
+    echo "Unknown source type '$SOURCE'." 1>&2
+    VALID_ARGUMENTS=0
+fi
 
-        if [[ "$EXEC_NAME" != *"emu"* ]]
-        then
-            ARGS="$ARGS -DHARDWARE -reuse-exe=$1 -Xshardware"
+# FPGA-specific options
+if [[ "$BACKEND" == "mono"  || "$BACKEND" == "tiling" ]]
+then
+    ARGS="$ARGS -fintelfpga -Xsv"
+
+    if [[ "$MODIFIER" != "emu" ]]
+    then
+        ARGS="$ARGS -DHARDWARE -reuse-exe=$EXEC_NAME -Xshardware"
             
-            if [[ -n $AOCL_BOARD_PACKAGE_ROOT ]]
-            then
-                ARGS="$ARGS -Xsboard=$FPGA_BOARD_NAME -Xsboard-package=$AOCL_BOARD_PACKAGE_ROOT"
-            fi
+        if [[ -n $AOCL_BOARD_PACKAGE_ROOT ]]
+        then
+            ARGS="$ARGS -Xsboard=$FPGA_BOARD_NAME -Xsboard-package=$AOCL_BOARD_PACKAGE_ROOT"
+        fi
 
-            if [[ "$EXEC_NAME" == *"report"* ]]
-            then
-                ARGS="$ARGS -fsycl-link"
-            fi
+        if [[ "$MODIFIER" == "report" ]]
+        then
+            ARGS="$ARGS -fsycl-link"
         fi
     fi
+fi
 
-    if [[ "$EXEC_NAME" == *"mono"* ]]
-    then
-        ARGS="$ARGS -DEXECUTOR=0"
-    elif [[ "$EXEC_NAME" == *"tiling"* ]]
-    then
-        ARGS="$ARGS -DEXECUTOR=1"
-    elif [[ "$EXEC_NAME" == *"cpu"* ]]
-    then
-        ARGS="$ARGS -DEXECUTOR=2"
-    fi
+if [[ "$BACKEND" == "mono" ]]
+then
+    ARGS="$ARGS -DEXECUTOR=0"
+elif [[ "$BACKEND" == "tiling" ]]
+then
+    ARGS="$ARGS -DEXECUTOR=1"
+elif [[ "$BACKEND" == "cpu" ]]
+then
+    ARGS="$ARGS -DEXECUTOR=2"
+else
+    echo "Unknown backend '$BACKEND'." 1>&2
+    VALID_ARGUMENTS=0
+fi
 
-    COMMAND="dpcpp src/*.cpp -o $EXEC_NAME $ARGS"
-    echo $COMMAND
-    echo $COMMAND | bash
-}
+if [[ $VALID_ARGUMENTS == 0 ]]
+then
+    echo "Execute '$0 -h' to show the usage of this script." 1>&2
+    exit 1
+fi
 
-run_build $1
+EXEC_NAME="fdtd_${MATERIAL}_${SOURCE}_${BACKEND}"
+if [[ -n $MODIFIER ]]
+then
+    EXEC_NAME="${EXEC_NAME}_${MODIFIER}"
+fi
+
+COMMAND="dpcpp src/*.cpp -o $EXEC_NAME $ARGS"
+echo $COMMAND
+echo $COMMAND | bash
