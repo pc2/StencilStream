199ee81756e25a6f5f6d4413e44c5cb4c754a637
Jan-Oliver "Janonard" Opdenhövel <joo@mail.uni-paderborn.de>
2022-07-18 11:29:28 +0200

    Switching to a fork of the Rodinia benchmark suite

--- examples/hotspot/scripts/build_rodinia.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ ! -d rodinia_3.1 ]
-then
-    curl http://www.cs.virginia.edu/~kw5na/lava/Rodinia/Packages/Current/rodinia_3.1.tar.bz2 | tar -xjf -
-fi
-
-make -C rodinia_3.1/openmp/hotspot hotspot
-cp rodinia_3.1/openmp/hotspot/hotspot hotspot_rodinia
