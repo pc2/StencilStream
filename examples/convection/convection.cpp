74ffccd52da5cea3126c43903781e97b44348a96
Jan-Oliver "Janonard" Opdenhövel <joo@mail.uni-paderborn.de>
2023-01-11 16:12:59 +0100

    Starting to flesh out the application

--- examples/convection/convection.cpp
+++ examples/convection/convection.cpp
@@ -4,12 +4,18 @@
 using namespace stencil;
 
 struct ThermalConvectionCell {
-    float T, Pt, Vx, Vy, Rx, Ry;
-    float tau_xx, tau_yy, sigma_xy;
-    float eta;
-    float dVxd_tau, dVyd_tau;
-    float ErrV, ErrP;
-    float qTx, qTy, dT_dt;
+
+    ThermalConvectionCell()
+        : T(0.0), Pt(0.0), Vx(0.0), Vy(0.0), Rx(0.0), Ry(0.0), tau_xx(0.0), tau_yy(0.0),
+          sigma_xy(0.0), eta(0.0), dVxd_tau(0.0), dVyd_tau(0.0), ErrV(0.0), ErrP(0.0), qTx(0.0),
+          qTy(0.0), dT_dt(0.0) {}
+
+    double T, Pt, Vx, Vy, Rx, Ry;
+    double tau_xx, tau_yy, sigma_xy;
+    double eta;
+    double dVxd_tau, dVyd_tau;
+    double ErrV, ErrP;
+    double qTx, qTy, dT_dt;
 };
 
 #define D_XA(FIELD) (stencil[ID(1, 0)].FIELD - stencil[ID(0, 0)].FIELD)
@@ -29,17 +35,16 @@ class PseudoTransientKernel {
     static constexpr uindex_t n_subgenerations = 5;
     using TimeDependentValue = std::monostate;
 
-    float roh0_g_alpha;
-    float delta_eta_delta_T;
-    float eta0;
-    float deltaT;
-    float dx, dy;
-    float Pt;
-    float delta_tau_iter;
-    float beta;
-    float rho;
-    float dampX, dampY;
-    float DcT;
+    double roh0_g_alpha;
+    double delta_eta_delta_T;
+    double eta0;
+    double deltaT;
+    double dx, dy;
+    double delta_tau_iter;
+    double beta;
+    double rho;
+    double dampX, dampY;
+    double DcT;
 
     Cell operator()(Stencil<Cell, 2> const &stencil) const {
         Cell new_cell = stencil[ID(0, 0)];
@@ -51,7 +56,7 @@ class PseudoTransientKernel {
 
             // compute_1!(...) (part 1 of 2)
             new_cell.eta = eta0 * (1.0 - delta_eta_delta_T * (stencil[ID(0, 0)].T + deltaT / 2.0));
-            float delta_V = D_XA(Vx) / dx + D_YA(Vy) / dy;
+            double delta_V = D_XA(Vx) / dx + D_YA(Vy) / dy;
             new_cell.Pt -= delta_tau_iter / beta * delta_V;
             new_cell.tau_xx = 2.0 * new_cell.eta * (D_XA(Vx) / dx - 1.0 / 3.0 * delta_V);
             new_cell.tau_yy = 2.0 * new_cell.eta * (D_YA(Vy) / dy - 1.0 / 3.0 * delta_V);
@@ -125,7 +130,122 @@ class PseudoTransientKernel {
 };
 
 int main() {
-    SimpleCPUExecutor<PseudoTransientKernel, tdv::NoneSupplier> executor(ThermalConvectionCell{},
-                                                                         PseudoTransientKernel());
+    double ly = 1.0;     // domain extend, m
+    double eta0 = 1.0;   // viscosity, Pa*s
+    double DcT = 1.0;    // heat diffusivity, m^2/s
+    double deltaT = 1.0; // initial temperature perturbation K
+
+    // Physics - nondim numbers
+    double Ra = 1e7;  // Raleigh number = ρ0*g*α*ΔT*ly^3/η0/DcT
+    double Pra = 1e3; // Prandtl number = η0/ρ0/DcT
+    double ar = 3;    // aspect ratio
+
+    // Physics - dimentionally dependent parameters
+    double lx = ar * ly;  // domain extend, m
+    double w = 1e-2 * ly; // initial perturbation standard deviation, m
+    double roh0_g_alpha = Ra * eta0 * DcT / deltaT / (ly * ly * ly); // thermal expansion
+    double delta_eta_delta_T = 1e-10 / deltaT; // viscosity's temperature dependence
+
+    // Numerics
+    double nx = 96 * ar - 1;
+    double ny = 96 - 1;     // numerical grid resolutions
+    double iterMax = 50000; // maximal number of pseudo-transient iterations
+    double nt = 3000;       // total number of timesteps
+    double nout = 10;       // frequency of plotting
+    double nerr = 100;      // frequency of error checking
+    double epsilon = 1e-4;  // nonlinear absolute tolerence
+    double dmp = 2;         // damping paramter
+    double st = 5;          // quiver plotting spatial step
+
+    // Derived numerics
+    double dx = lx / (nx - 1);
+    double dy = ly / (ny - 1);           // cell size
+    double rho = 1.0 / Pra * eta0 / DcT; // density
+    double dt_diff =
+        1.0 / 4.1 * std::pow(std::min(dx, dy), 2) / DcT; // diffusive CFL timestep limiter
+    double delta_tau_iter = 1.0 / 6.1 * std::min(dx, dy) /
+                            std::sqrt(eta0 / rho); // iterative CFL pseudo-timestep limiter
+    double beta = 6.1 * std::pow(delta_tau_iter, 2) / std::pow(std::min(dx, dy), 2) /
+                  rho;             // numerical bulk compressibility
+    double dampX = 1.0 - dmp / nx; // damping term for the x-momentum equation
+    double dampY = 1.0 - dmp / ny; // damping term for the y-momentum equation
+
+    PseudoTransientKernel kernel{
+        .roh0_g_alpha = roh0_g_alpha,
+        .delta_eta_delta_T = delta_eta_delta_T,
+        .eta0 = eta0,
+        .deltaT = deltaT,
+        .dx = dx,
+        .dy = dy,
+        .delta_tau_iter = delta_tau_iter,
+        .beta = beta,
+        .rho = rho,
+        .dampX = dampX,
+        .dampY = dampY,
+        .DcT = DcT,
+    };
+
+    cl::sycl::buffer<ThermalConvectionCell, 2> grid = cl::sycl::range<2>(nx + 1, ny + 1);
+    {
+        auto ac = grid.template get_access<cl::sycl::access::mode::discard_write>();
+        for (uint32_t x = 0; x < nx + 1; x++) {
+            for (uint32_t y = 0; y < ny + 1; y++) {
+                ThermalConvectionCell cell;
+                cell.T = deltaT * std::exp(-std::pow((x * dx - 0.5 * lx) / w, 2) -
+                                           std::pow((y * dy - 0.5 * ly) / w, 2));
+                ac[x][y] = cell;
+            }
+        }
+    }
+
+    SimpleCPUExecutor<PseudoTransientKernel, tdv::NoneSupplier> executor(ThermalConvectionCell(),
+                                                                         kernel);
+    executor.set_input(grid);
+
+    for (uint32_t it = 0; it < 1; it++) {
+        double errV = 2 * epsilon;
+        double errP = 2 * epsilon;
+        double max_ErrV, max_ErrP, max_Vx, max_Vy, max_Pt;
+        executor.set_i_generation(0);
+
+        while ((errV > epsilon || errP > epsilon) && executor.get_i_generation() < iterMax) {
+            executor.run(nerr);
+            executor.copy_output(grid);
+
+            max_ErrV = max_ErrP = max_Vx = max_Vy = max_Pt =
+                -std::numeric_limits<double>::infinity();
+            {
+                auto ac = grid.template get_access<cl::sycl::access::mode::read>();
+                for (uint32_t x = 0; x < nx; x++) {
+                    for (uint32_t y = 0; y < ny; y++) {
+                        auto cell = ac[x][y];
+                        if (std::abs(cell.ErrV) > max_ErrV) {
+                            max_ErrV = std::abs(cell.ErrV);
+                        }
+                        if (std::abs(cell.ErrP) > max_ErrP) {
+                            max_ErrP = std::abs(cell.ErrP);
+                        }
+                        if (std::abs(cell.Vx) > max_Vx) {
+                            max_Vx = std::abs(cell.Vx);
+                        }
+                        if (std::abs(cell.Vy) > max_Vy) {
+                            max_Vy = std::abs(cell.Vy);
+                        }
+                        if (std::abs(cell.Pt) > max_Pt) {
+                            max_Pt = std::abs(cell.Pt);
+                        }
+                    }
+                }
+            }
+            errV = max_ErrV / (1e-12 + max_Vy);
+            errP = max_ErrP / (1e-12 + max_Pt);
+        }
+
+        double dt_adv = std::min(dx / max_Vx, dy / max_Vy) / 2.1;
+        double dt = std::min(dt_diff, dt_adv);
+
+        std::cout << executor.get_i_generation() << " " << max_ErrV << " " << max_ErrP << std::endl;
+        // update T kernel
+    }
     return 0;
 }
