24c276c8b626d74d53933eaf853918e4f0acf4d2
Jan-Oliver Opdenhövel <joo@mail.uni-paderborn.de>
2023-08-13 11:31:48 +0200

    Making the release script dynamic for different apps

---
+++ scripts/publish-release.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+set -e
+
+module reset
+module load lang Julia
+
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <release name> <tag name> <commit sha>" &1>2
+    exit 1
+fi
+
+RELEASE_NAME=$1
+TAG_NAME=$2
+CI_COMMIT_SHA=$3
+
+# Cloning the wiki and adding the new performance metrics.
+git clone git@git.uni-paderborn.de:pc2/sycl-stencil.wiki.git wiki
+cd wiki
+for metrics_file in ../examples/*/*.json
+do
+    ./Performance-tracking/add_data.jl $TAG_NAME $metrics_file
+done
+./Performance-tracking/render-site.jl > Performance-tracking.md
+git stage Performance-tracking.md Performance-tracking/data.csv
+git commit -m "Adding performance data of $TAG_NAME"
+git push
+cd ..
+
+# Downloading the release cli tool
+curl -L https://gitlab.com/gitlab-org/release-cli/-/releases/v0.15.0/downloads/bin/release-cli-linux-amd64 > release-cli
+echo "d59169bab5dfe4693af4f181b08bf11ef1c96a4da30eff0f04abb236c54e62e9  release-cli" | sha256sum -c || exit 1
+chmod +x release-cli
+
+# Preparing the assets links
+ASSETS_LINK_FILE=$(mktemp)
+echo "{" > $ASSETS_LINK_FILE
+
+for package in $(ls build/examples/*/*.tar.gz)
+do
+    FILE_NAME=$(basename $package)
+    PACKAGE_NAME=$(basename -s .tar.gz $package)
+    echo "{\"name\":\"${FILE_NAME}\",\"url\":\"${PACKAGE_REGISTRY_URL}/${PACKAGE_NAME}/${TAG_NAME}/${FILE_NAME}\"}," >> $ASSETS_LINK_FILE
+done
+
+echo "}" >> $ASSETS_LINK_FILE
+
+# Creating the release
+./release-cli create
+    --name "${RELEASE_NAME}" --tag-name "${TAG_NAME}" --ref "${CI_COMMIT_SHA}"
+    --assets-link $(cat $ASSETS_LINK_FILE)
+
+# Cleanup
+rm $ASSETS_LINK_FILE
