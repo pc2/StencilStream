223ba314ef6ee8b7c7f52a4289326e282ad41f2c
Jan-Oliver Opdenhövel <joo@mail.uni-paderborn.de>
2023-08-15 10:09:59 +0200

    Trying to fix the package link collection

--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -5,13 +5,9 @@ module reset
 module load lang Julia
 
 if [ $# -ne 3 ]; then
-    echo "Usage: $0 <release name> <tag name> <commit sha>" &1>2
+    echo "Usage: $0 <release name> <tag name> <commit sha>" 1>&2
     exit 1
 fi
-
-RELEASE_NAME=$1
-TAG_NAME=$2
-CI_COMMIT_SHA=$3
 
 # Cloning the wiki and adding the new performance metrics.
 git clone git@git.uni-paderborn.de:pc2/sycl-stencil.wiki.git wiki
@@ -33,21 +29,18 @@ chmod +x release-cli
 
 # Preparing the assets links
 ASSETS_LINK_FILE=$(mktemp)
-echo "{" > $ASSETS_LINK_FILE
 
 for package in $(ls build/examples/*/*.tar.gz)
 do
     FILE_NAME=$(basename $package)
     PACKAGE_NAME=$(basename -s .tar.gz $package)
-    echo "{\"name\":\"${FILE_NAME}\",\"url\":\"${PACKAGE_REGISTRY_URL}/${PACKAGE_NAME}/${TAG_NAME}/${FILE_NAME}\"}," >> $ASSETS_LINK_FILE
+    echo "{\"name\":\"${FILE_NAME}\", \"url\":\"${PACKAGE_REGISTRY_URL}/${PACKAGE_NAME}/${TAG_NAME}/${FILE_NAME}\"}" >> $ASSETS_LINK_FILE
 done
 
-echo "}" >> $ASSETS_LINK_FILE
-
 # Creating the release
-./release-cli create
-    --name "${RELEASE_NAME}" --tag-name "${TAG_NAME}" --ref "${CI_COMMIT_SHA}"
-    --assets-link $(cat $ASSETS_LINK_FILE)
+./release-cli create \
+    --name "${RELEASE_NAME}" --tag-name "${TAG_NAME}" --ref "${CI_COMMIT_SHA}" \
+    --assets-link="[$(cat $ASSETS_LINK_FILE | tr "\n" ", " | sed 's/,$/\n/')]"
 
 # Cleanup
 rm $ASSETS_LINK_FILE
