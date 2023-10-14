cc43745ae1fa9dca9b0f2d6608b1007e1a9e5cfb
Jan-Oliver Opdenhövel <joo@mail.uni-paderborn.de>
2023-10-14 10:04:19 +0200

    Also printing julia depot path in env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -3,3 +3,4 @@ module reset
 module load lang fpga devel intel/oneapi/23.2.0 bittware/520n/20.4.0_hpc Boost/1.81.0-GCC-12.2.0 CMake/3.24.3-GCCcore-12.2.0 Julia/1.9.3-linux-x86_64
 icpx --version
 julia --version
+echo "julia depot path: $JULIA_DEPOT_PATH"
