66634e389f06e4be63f2cb7b6fb8b68be2c19be8
Jan-Oliver Opdenhövel <joo@mail.uni-paderborn.de>
2024-08-07 11:45:24 +0200

    Starting to document some concepts

--- StencilStream/Concepts.hpp
+++ StencilStream/Concepts.hpp
@@ -29,22 +29,38 @@
 
 namespace stencil {
 namespace concepts {
+
+/**
+ * \brief A technical definition of a stencil transition function.
+ * 
+ * This concept lists all required type definitions, constants, and methods that a \ref stencil::concepts::StencilUpdate "StencilUpdate" needs from a transition function.
+ * This includes multiple definitions for advanced features.
+ * Most users are adviced to extend the \ref stencil::BaseTransitionFunction class.
+ * It provides default definitions that disable those features.
+ * 
+ * The required type definitions are:
+ * * `Cell`: The cell type the transition function operates on. It must be semiregular, i.e. copyable bit by bit.
+ * * `TimeDependentValue`: The type of the time-dependent value computed by the `get_time_dependent_value` method. It must also be semiregular.
+ * 
+ * The required constants are:
+ * * `uindex_t stencil_radius`: The radius of the stencil. It must be greater than or equal to 1.
+ * * `uindex_t n_subiterations`: The number of sub-iterations of the transition function. It must be greater than or equal to 1.
+ * 
+ * The required methods are:
+ * * `Cell operator()(Stencil<Cell, stencil_radius> const&stencil) const`: Compute the next iteration of the stencil's central cell. This method must be pure, i.e. it must not modify either the stencil's or the transition function's state.
+ * * `TimeDependentValue get_time_dependent_value(uindex_t i_iteration) const`: Compute the time-dependent value for the given iteration. This method must be pure, i.e. it must not modify the transition function's state.
+ */
 template <typename T>
 concept TransitionFunction =
-    // Cells must to be semiregular.
     std::semiregular<typename T::Cell> &&
-    // Time-dependent values must be copyable.
     std::copyable<typename T::TimeDependentValue> &&
-    // The stencil radius must be a constant greater than 1.
+    
     std::same_as<decltype(T::stencil_radius), const uindex_t> && (T::stencil_radius >= 1) &&
-    // The number of sub-iterations must be a constant greater than 1.
     std::same_as<decltype(T::n_subiterations), const uindex_t> && (T::n_subiterations >= 1) &&
-    // The transition function must be invocable. Its argument must be a stencil
-    // and its return value must be a cell.
+    
     requires(T const &trans_func,
              Stencil<typename T::Cell, T::stencil_radius, typename T::TimeDependentValue> const
                  &stencil) {
-        // update method
         { trans_func(stencil) } -> std::same_as<typename T::Cell>;
     } &&
     requires(T const &trans_func, uindex_t i_iteration) {
@@ -53,12 +69,24 @@ concept TransitionFunction =
         } -> std::same_as<typename T::TimeDependentValue>;
     };
 
+/**
+ * \brief An accessor for a two-dimensional grid.
+ * 
+ * It must provide access either via a `sycl::id<2>` object or via two consecutive accesses with `uindex_t`s.
+ */
 template <typename Accessor, typename Cell>
 concept GridAccessor = requires(Accessor ac, uindex_t c, uindex_t r) {
     { ac[sycl::id<2>(c, r)] } -> std::same_as<Cell &>;
     { ac[c][r] } -> std::same_as<Cell &>;
 };
 
+/**
+ * \brief A container for cells.
+ * 
+ * This concept defines certain methods and properties that all grids should share, so that backend-independent applications can be written.
+ * 
+ * First of all, each grid must contain a template class called `GridAccessor` that, given an instance of `sycl::access::mode`, fulfills the \ref stencil::concepts::GridAccessor "GridAccessor" concept.
+ */
 template <typename G, typename Cell>
 concept Grid = requires(G &grid, sycl::buffer<Cell, 2> buffer, uindex_t c, uindex_t r, Cell cell) {
     { G(c, r) } -> std::same_as<G>;
