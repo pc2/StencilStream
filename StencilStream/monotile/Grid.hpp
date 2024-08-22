9856ffb2cda2241cb12d039970966f7d1d812c69
Jan-Oliver Opdenhövel <joo@mail.uni-paderborn.de>
2024-08-22 15:16:34 +0200

    Documenting monotile's grid class

--- StencilStream/monotile/Grid.hpp
+++ StencilStream/monotile/Grid.hpp
@@ -24,36 +24,112 @@
 namespace stencil {
 namespace monotile {
 
+/**
+ * \brief A grid class for the monotile architecture
+ *
+ * This grid, which fullfils the \ref stencil::concepts::Grid "Grid" concept, contains a
+ * two-dimensional buffer of cells to be used together with the \ref StencilUpdate class.
+ *
+ * The contents of the grid can be accessed by a host-side program using the \ref GridAccessor class
+ * template. For example, one can write the contents of a `grid` object as follows:
+ *
+ * ```
+ * Grid::GridAccessor<sycl::access::mode::read_write> accessor(grid);
+ * for (uindex_t c = 0; c < grid.get_grid_width(); c++) {
+ *     for (uindex_t r = 0; r < grid.get_grid_height(); r++) {
+ *         accessor[c][r] = foo(c, r);
+ *     }
+ * }
+ * ```
+ *
+ * Alternatively, one may write their data into a SYCL buffer and copy it into the grid using the
+ * method \ref copy_from_buffer. The method \ref copy_to_buffer does the reverse: It writes the
+ * contents of the grid into a SYCL buffer.
+ *
+ * On the device side, the data can be read or written with the help of the method templates \ref
+ * submit_read and \ref submit_write. Those take a SYCL pipe as a template argumment and enqueue
+ * kernels that read/write the contents of the grid to/from the pipes.
+ *
+ * \tparam Cell The cell type to store.
+ *
+ * \tparam word_size The word size of the memory system, in bytes. This is used to optimize the
+ * kernels submitted by \ref submit_read and \ref submit_write.
+ */
 template <class Cell, uindex_t word_size = 64> class Grid {
-  public:
-    static constexpr uindex_t dimensions = 2;
+  private:
     static constexpr uindex_t word_length =
         std::lcm(sizeof(Padded<Cell>), word_size) / sizeof(Padded<Cell>);
-
     using IOWord = std::array<Padded<Cell>, word_length>;
 
+  public:
+    /**
+     * \brief The number of dimensions of the grid.
+     *
+     * May be changed in the future when other dimensions are supported.
+     */
+    static constexpr uindex_t dimensions = 2;
+
+    /**
+     * \brief Create a new, uninitialized grid with the given dimensions.
+     *
+     * \param grid_width The width, or number of columns, of the new grid.
+     *
+     * \param grid_height The height, or number of rows, of the new grid.
+     */
     Grid(uindex_t grid_width, uindex_t grid_height)
         : tile_buffer(sycl::range<1>(n_cells_to_n_words(grid_width * grid_height, word_length))),
           grid_width(grid_width), grid_height(grid_height) {}
 
+    /**
+     * \brief Create a new, uninitialized grid with the given dimensions.
+     *
+     * \param range The range of the new grid. The first index will be the width and the second
+     * index will be the height of the grid.
+     */
     Grid(sycl::range<2> range)
         : tile_buffer(sycl::range<1>(n_cells_to_n_words(range[0] * range[1], word_length))),
           grid_width(range[0]), grid_height(range[1]) {}
 
+    /**
+     * \brief Create a new grid with the same size and contents as the given SYCL buffer.
+     *
+     * The contents of the buffer will be copied to the grid by the host. The SYCL buffer can later
+     * be used elsewhere.
+     *
+     * \param buffer The buffer with the contents of the new grid.
+     */
     Grid(sycl::buffer<Cell, 2> buffer)
         : tile_buffer(1), grid_width(buffer.get_range()[0]), grid_height(buffer.get_range()[1]) {
         tile_buffer = sycl::range<1>(n_cells_to_n_words(grid_width * grid_height, word_length));
         copy_from_buffer(buffer);
     }
 
+    /**
+     * \brief Create a new reference to the given grid.
+     *
+     * The newly created grid object will point to the same underlying data as the referenced grid.
+     * Changes made via the newly created grid object will also be visible to the old grid object,
+     * and vice-versa.
+     *
+     * \param other_grid The other grid the new grid should reference.
+     */
     Grid(Grid const &other_grid)
         : tile_buffer(other_grid.tile_buffer), grid_width(other_grid.grid_width),
           grid_height(other_grid.grid_height) {}
 
+    /**
+     * \brief Create an new, uninitialized grid with the same size as the current one.
+     */
     Grid make_similar() const { return Grid(grid_width, grid_height); }
 
+    /**
+     * \brief Return the width, or number of columns, of the grid.
+     */
     uindex_t get_grid_width() const { return grid_width; }
 
+    /**
+     * \brief Return the height, or number of rows, of the grid.
+     */
     uindex_t get_grid_height() const { return grid_height; }
 
     template <sycl::access::mode access_mode = sycl::access::mode::read_write> class GridAccessor {
@@ -89,6 +165,16 @@ template <class Cell, uindex_t word_size = 64> class Grid {
         uindex_t grid_width, grid_height;
     };
 
+    /**
+     * \brief Copy the contents of the SYCL buffer into the grid.
+     *
+     * The SYCL buffer will be accessed read-only one the host; It may be used elsewhere too. The
+     * buffer however has to have the same size as the grid, otherwise a \ref std::range_error is
+     * thrown.
+     *
+     * \param input_buffer The buffer to copy the data from.
+     * \throws std::range_error The size of the buffer does not match the grid.
+     */
     void copy_from_buffer(sycl::buffer<Cell, 2> input_buffer) {
         uindex_t width = this->get_grid_width();
         uindex_t height = this->get_grid_height();
@@ -106,6 +192,15 @@ template <class Cell, uindex_t word_size = 64> class Grid {
         }
     }
 
+    /**
+     * \brief Copy the contents of the grid into the SYCL buffer.
+     *
+     * The contents of the SYCL buffer will be overwritten on the host. The buffer also has to have
+     * the same size as the grid, otherwise a \ref std::range_error is thrown.
+     *
+     * \param output_buffer The buffer to copy the data to.
+     * \throws std::range_error The size of the buffer does not match the grid.
+     */
     void copy_to_buffer(sycl::buffer<Cell, 2> output_buffer) {
         uindex_t width = this->get_grid_width();
         uindex_t height = this->get_grid_height();
@@ -123,8 +218,24 @@ template <class Cell, uindex_t word_size = 64> class Grid {
         }
     }
 
-    template <typename in_pipe> void submit_read(sycl::queue queue) {
-        queue.submit([&](sycl::handler &cgh) {
+    /**
+     * \brief Submit a kernel that sends the contents of the grid into the pipe.
+     *
+     * The entirety of the grid will be send into the pipe in column-major order, meaning that the
+     * last index (which denotes the row) will change the quickest. The method returns the event of
+     * the launched kernel immediately.
+     *
+     * This method is explicitly part of the user-facing API: You are allowed and encouraged to use
+     * this method to feed custom kernels.
+     *
+     * \tparam in_pipe The pipe the data is written to.
+     *
+     * \param queue The queue to submit the kernel to.
+     *
+     * \returns The event object of the submitted kernel.
+     */
+    template <typename in_pipe> sycl::event submit_read(sycl::queue queue) {
+        return queue.submit([&](sycl::handler &cgh) {
             sycl::accessor ac(tile_buffer, cgh, sycl::read_only);
             uindex_t n_cells = grid_width * grid_height;
 
@@ -146,8 +257,25 @@ template <class Cell, uindex_t word_size = 64> class Grid {
         });
     }
 
-    template <typename out_pipe> void submit_write(sycl::queue queue) {
-        queue.submit([&](sycl::handler &cgh) {
+    /**
+     * \brief Submit a kernel that receives cells from the pipe and writes them to the grid.
+     *
+     * The kernel expects that the entirety of the grid can be overwritten with the cells read from
+     * the pipe. Also, it expects that the cells are sent in column-major order, meaning that the
+     * last index (which denotes the row) will change the quickest. The method returns the event of
+     * the launched kernel immediately.
+     *
+     * This method is explicitly part of the user-facing API: You are allowed and encouraged to use
+     * this method to feed custom kernels.
+     *
+     * \tparam out_pipe The pipe the data is received from.
+     *
+     * \param queue The queue to submit the kernel to.
+     *
+     * \returns The event object of the submitted kernel.
+     */
+    template <typename out_pipe> sycl::event submit_write(sycl::queue queue) {
+        return queue.submit([&](sycl::handler &cgh) {
             sycl::accessor ac(tile_buffer, cgh, sycl::write_only);
             uindex_t n_cells = grid_width * grid_height;
 
