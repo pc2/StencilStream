f31ab3f7719c5f99f9159a9309a58c2c36e50ea9
Jan-Oliver Opdenhövel <joo@mail.uni-paderborn.de>
2024-11-05 11:10:49 +0100

    Removing the dependency on Boost

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 module reset
-ml lang fpga devel intel/oneapi/23.2.0 bittware/520n/20.4.0_hpc Boost/1.77.0-GCC-11.2.0 CMake/3.22.1-GCCcore-11.2.0 Julia/1.10.4
+ml lang fpga devel intel/oneapi/23.2.0 bittware/520n/20.4.0_hpc CMake/3.22.1-GCCcore-11.2.0 Julia/1.10.4
 icpx --version
 julia --version
 echo "julia depot path: $JULIA_DEPOT_PATH"
