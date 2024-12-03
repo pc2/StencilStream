862e3ac54ca1ef3d7d7e0e00355749be5c9e4763
tstoehr <tstoehr@mail.uni-paderborn.de>
2024-12-03 10:20:43 +0100

    Change namings from gpu to cuda

--- examples/diffusion/diffusion.cpp
+++ examples/diffusion/diffusion.cpp
@@ -1,9 +1,7 @@
 #include <StencilStream/BaseTransitionFunction.hpp>
 #include <StencilStream/cpu/StencilUpdate.hpp>
-#include <StencilStream/gpu/StencilUpdate.hpp>
-#include <StencilStream/monotile/StencilUpdate.hpp>
-#include <sycl/ext/intel/fpga_extensions.hpp>
-#include "CL/sycl.hpp"
+#include <StencilStream/cuda/StencilUpdate.hpp>
+#include "sycl/sycl.hpp"
 
 #include <chrono>
 #include <cstdio>
@@ -12,12 +10,13 @@
 #include <vector>
 #include <string>
 
+using namespace std;
 using wall_clock_t = std::chrono::high_resolution_clock;
 using namespace stencil;
 #if defined(STENCILSTREAM_BACKEND_CPU)
 using namespace stencil::cpu;
-#elif (STENCILSTREAM_BACKEND_GPU)
-using namespace stencil::gpu;
+#elif (STENCILSTREAM_BACKEND_CUDA)
+using namespace stencil::cuda;
 #else
 using namespace stencil::monotile;
 #endif
@@ -46,19 +45,28 @@ struct HeatKernel : public BaseTransitionFunction
         auto [c, r] = stencil.id;
 
         // Boundary conditions
-        if (r == 0) {
+        if (r == 0)
+        {
             // Top boundary
             return 85.0;
-        } else if (r == rows - 1) {
+        }
+        else if (r == rows - 1)
+        {
             // Bottom boundary
             return 5.0;
-        } else if (c == 0) {
+        }
+        else if (c == 0)
+        {
             // Left boundary
             return 20.0;
-        } else if (c == cols - 1) {
+        }
+        else if (c == cols - 1)
+        {
             // Right boundary
             return 70.0;
-        } else {
+        }
+        else
+        {
             // Stencil design
             double center = stencil[ID(0, 0)];
             double left = stencil[ID(-1, 0)];
@@ -67,8 +75,7 @@ struct HeatKernel : public BaseTransitionFunction
             double bottom = stencil[ID(0, 1)];
 
             // Calculation
-            return center + a * dt * ((left - 2.0 * center + right) / (DX * DX) +
-                                      (top - 2.0 * center + bottom) / (DY * DY));
+            return center + a * dt * ((left - 2.0 * center + right) / (DX * DX) + (top - 2.0 * center + bottom) / (DY * DY));
         }
     }
 };
@@ -89,12 +96,12 @@ void initialize(field *field, uindex_t rows, uindex_t cols)
     field->data.resize(newSize, 0.0);
 
     radius = field->nx / 6.0;
-    for (int i = 0; i < field->nx ; i++)
+    for (int i = 0; i < field->nx; i++)
     {
-        for (int j = 0; j < field->ny ; j++)
+        for (int j = 0; j < field->ny; j++)
         {
             ind = i * field->ny + j;
-            
+
             dx = i - field->nx / 2;
             dy = j - field->ny / 2;
             if (dx * dx + dy * dy < radius * radius)
@@ -109,65 +116,72 @@ void initialize(field *field, uindex_t rows, uindex_t cols)
     }
 
     // Boundary conditions
-    for (int i = 0; i < field->nx ; i++)
+    for (int i = 0; i < field->nx; i++)
     {
         field->data[i * field->ny + field->ny - 1] = 70.0;
-        field->data[i * field->ny ] = 20.0;
+        field->data[i * field->ny] = 20.0;
     }
 
-    for (int j = 0; j < field->ny ; j++)
+    for (int j = 0; j < field->ny; j++)
     {
         field->data[j] = 85.0;
     }
-    for (int j = 0; j < field->ny ; j++)
+    for (int j = 0; j < field->ny; j++)
     {
-        field->data[(field->nx - 1)* field->ny + j] = 5.0;
+        field->data[(field->nx - 1) * field->ny + j] = 5.0;
     }
 }
 
-//void print_field(field *field)
+// void print_field(field *field)
 //{
-    //int stride = field->ny;
+// int stride = field->ny;
 
-    //for (int i = 0; i < field->nx; i++)
-    //{
-        //for (int j = 0; j < field->ny; j++) 
-        //{
-            //std::cout << field->data[(i * stride) + j] << " ";
-        //}
-        //std::cout << std::endl;
-    //}
+// for (int i = 0; i < field->nx; i++)
+//{
+// for (int j = 0; j < field->ny; j++)
+//{
+// std::cout << field->data[(i * stride) + j] << " ";
+//}
+// std::cout << std::endl;
+//}
 //}
 
-void write(Grid<double> output_grid) {
+void write(Grid<double> output_grid)
+{
     // TODO: Change output naming
-    // TODO: Add a loop for naming 
+    // TODO: Add a loop for naming
     std::string name = "Output.csv";
     std::ofstream myFile(name);
 
     Grid<double>::GridAccessor<sycl::access::mode::read> grid_ac(output_grid);
 
-    for (uindex_t r = 0; r < output_grid.get_grid_height(); r++) {
-        for (uindex_t c = 0; c < output_grid.get_grid_width(); c++) {
-            //std::cout << grid_ac[c][r] << ' ';
+    for (uindex_t r = 0; r < output_grid.get_grid_height(); r++)
+    {
+        for (uindex_t c = 0; c < output_grid.get_grid_width(); c++)
+        {
+            // std::cout << grid_ac[c][r] << ' ';
             myFile << grid_ac[c][r];
-            if(c + 1 != output_grid.get_grid_width()) {
+            if (c + 1 != output_grid.get_grid_width())
+            {
                 myFile << ",";
             }
         }
-        //std::cout << std::endl;
+        // std::cout << std::endl;
         myFile << std::endl;
     }
 }
 
-Grid<double> read(field *field, uindex_t rows, uindex_t cols) {
+Grid<double> read(field *field, uindex_t rows, uindex_t cols)
+{
     Grid<double> input_grid(rows, cols);
     {
         Grid<double>::GridAccessor<sycl::access::mode::read_write> grid_ac(input_grid);
 
-        uindex_t stride = input_grid.get_grid_height(); 
-        for (uindex_t r = 0; r < cols; r++) {
-            for (uindex_t c = 0; c < rows; c++) {
+        uindex_t stride = input_grid.get_grid_height();
+        for (uindex_t r = 0; r < cols; r++)
+        {
+            for (uindex_t c = 0; c < rows; c++)
+            {
                 grid_ac[c][r] = field->data[r * stride + c];
             }
         }
@@ -175,26 +189,32 @@ Grid<double> read(field *field, uindex_t rows, uindex_t cols) {
     return input_grid;
 }
 
-//void printField(field *field){
-    //int stride = field->ny; // Total number of columns, including ghost layers
+// void printField(field *field){
+// int stride = field->ny; // Total number of columns, including ghost layers
 
-    //for (int i = 0; i < field->nx; i++) // Loop over all rows, including ghost layers
-    //{
-        //for (int j = 0; j < field->ny; j++) // Loop over all columns, including ghost layers
-        //{
-            //std::cout << field->data[(i * stride) + j] << " ";
-        //}
-        //std::cout << std::endl;
-    //}
+// for (int i = 0; i < field->nx; i++) // Loop over all rows, including ghost layers
+//{
+// for (int j = 0; j < field->ny; j++) // Loop over all columns, including ghost layers
+//{
+// std::cout << field->data[(i * stride) + j] << " ";
+//}
+// std::cout << std::endl;
+//}
 //}
 
 int main(int argc, char **argv)
 {
     field field;
 
-    const uindex_t rows = atoi(argv[1]);
-    const uindex_t cols = atoi(argv[2]);
-    uindex_t nsteps = atoi(argv[3]);
+    if (argc != 4)
+    {
+        std::cerr << "Usage: " << argv[0] << " <width> <height> <n_iterations>" << std::endl;
+        return 1;
+    }
+
+    const uindex_t rows = stoi(argv[1]);
+    const uindex_t cols = stoi(argv[2]);
+    uindex_t nsteps = stoi(argv[3]);
 
     auto start = wall_clock_t::now();
 
@@ -216,9 +236,7 @@ int main(int argc, char **argv)
 
     Grid<double> grid = read(&field, rows, cols);
 
-#if defined(STENCILSTREAM_TARGET_FPGA)
-    sycl::device device(sycl::ext::intel::fpga_selector_v);
-#elif defined(STENCILSTREAM_TARGET_GPU)
+#if defined(STENCILSTREAM_TARGET_CUDA)
     sycl::device device(sycl::gpu_selector_v);
 #else
     sycl::device device;
@@ -230,13 +248,21 @@ int main(int argc, char **argv)
         .device = device,
         .blocking = true,
     });
-    grid = update(grid);
+
+    try
+    {
+        grid = update(grid);
+    }
+    catch (sycl::exception const &e)
+    {
+        std::cout << "Caught asynchronous SYCL exception:\n"
+                  << e.what() << std::endl;
+    }
 
     std::cout << "Ending simulation" << std::endl;
     std::cout << "Walltime: " << update.get_walltime() << " s" << std::endl;
 
-
-    write(grid); 
+    write(grid);
 
     return 0;
 }
