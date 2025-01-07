69b70ce41e47e7cd6dbc74e0badab7c5d2d21eab
tstoehr <tstoehr@mail.uni-paderborn.de>
2025-01-07 10:18:02 +0100

    Change CMake version

--- scripts/env_cuda.sh
+++ scripts/env_cuda.sh
@@ -3,8 +3,8 @@ module reset
 #ml lang fpga devel system intel/oneapi/25.0.0 CMake/3.29.3-GCCcore-13.3.0 CUDA/12.6.0
 
 ml fpga
-ml intel/oneapi/25.0.0
-ml devel/CMake/3.29.3-GCCcore-13.3.0
+ml intel/oneapi/23.2.0
+ml devel/CMake/3.22.1-GCCcore-11.2.0
 ml system/CUDA/12.6.0
 
 icpx --version
