febcd9c644dd32e97cefced77c5d39d1fa0ee0c4
Jan-Oliver Opdenhövel <joo@mail.uni-paderborn.de>
2025-01-20 10:31:37 +0100

    Going to 24.2.1 as the least bad, new oneAPI version

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 module reset
-ml lang fpga devel intel/oneapi/23.2.0 bittware/520n/20.4.0_hpc CMake/3.29.3-GCCcore-13.3.0 Julia/1.10.4
+ml lang fpga devel intel/oneapi/24.2.1 bittware/520n/20.4.0_hpc CMake/3.29.3-GCCcore-13.3.0 Julia/1.10.4
 icpx --version
 julia --version
 echo "julia depot path: $JULIA_DEPOT_PATH"
