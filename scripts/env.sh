11d786f01b1074f11ac8f749a47c0a2d40859892
Jan-Oliver Opdenhövel <joo@mail.uni-paderborn.de>
2025-01-20 10:11:23 +0100

    Revert "Also bumping to 25.0.0"
    This reverts commit ab95cc8755abffc36330d4e445561f67c3106c33.

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 module reset
-ml lang fpga devel intel/oneapi/25.0.0 bittware/520n/20.4.0_hpc CMake/3.22.1-GCCcore-11.2.0 Julia/1.10.4
+ml lang fpga devel intel/oneapi/24.2.1 bittware/520n/20.4.0_hpc CMake/3.22.1-GCCcore-11.2.0 Julia/1.10.4
 icpx --version
 julia --version
 echo "julia depot path: $JULIA_DEPOT_PATH"
