e46ff2a8faaa0be9a6c4774dfb7c19b9f5a226a5
tstoehr <tstoehr@mail.uni-paderborn.de>
2025-07-03 08:51:18 +0200

    Change classes to avoid concepts

--- StencilStream/AccessorSubscript.hpp
+++ StencilStream/AccessorSubscript.hpp
@@ -1,137 +1,44 @@
-/*
- * Copyright © 2020-2024 Jan-Oliver Opdenhövel, Paderborn Center for Parallel Computing, Paderborn
- * University
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
- * associated documentation files (the “Software”), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge, publish, distribute,
- * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or
- * substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
- * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 #pragma once
 #include <sycl/sycl.hpp>
 
-namespace stencil {
-/**
- * @brief A helper class to support the double-subscript idiom for
- * \ref stencil::concepts::GridAccessor "GridAccessors"
- *
- * This class is used to support expressions like `accessor[i_row][i_column]`. Each subscript except
- * the last one creates an object of this class, which stores the index of the subscript as well as
- * all previous indices to form a so-called "prefix." When the last subscript is called, the index
- * is added to the prefix to yield the complete, multi-dimensional index. Then, the last subscript
- * object returns a reference to the requested cell.
- *
- * \tparam Cell The cell type to support
- *
- * \tparam Accessor accessor to direct to. It must fulfill \ref
- * stencil::concepts::GridAccessor, but since this class is also part of the concept's definition,
- * we can't check it here.
- *
- * \tparam access_mode The access mode of the accessor. Used to enable and disable different \ref
- * operator[] implementations
- *
- * \tparam current_subdim The current subdim. Decides whether the next call to \ref
- * operator[] returns another \ref AccessorSubscript or the final cell reference.
- */
+// This accessor is shifted by 1. It is used for the padded access idea.
+
 template <typename Cell, typename Accessor, sycl::access::mode access_mode,
           std::size_t current_subdim = 0>
 class AccessorSubscript {
   public:
-    /// \brief The number of dimensions in the accessed grid.
     static constexpr std::size_t dimensions = Accessor::dimensions;
 
-    /**
-     * \brief Instantiate a new accessor subscript object with the given index as the prefix.
-     *
-     * This accessor is meant to be used by the `operator[]` implementation of the accessor to
-     * construct the first subscript object. It is therefore only available iff the current
-     * subdimention is zero.
-     *
-     * \param ac The accessor to redirect to.
-     * \param i The index of the previous subscript. It will be used as a prefix.
-     */
     AccessorSubscript(Accessor &ac, std::size_t i)
         requires(current_subdim == 0)
         : ac(ac), id_prefix() {
-        id_prefix[current_subdim] = i;
+        id_prefix[current_subdim] = i + 1;
     }
 
-    /**
-     * \brief Instantiate a new subscript object with the given prefix and subscript index.
-     *
-     * This constructor will take the given prefix, add the new index to it at the current position,
-     * and use the resulting prefix.
-     *
-     * \param ac The accessor to redirect to.
-     * \param id_prefix The previous prefix
-     * \param i The new index to add to the prefix.
-     */
     AccessorSubscript(Accessor &ac, sycl::id<dimensions> id_prefix, std::size_t i)
         : ac(ac), id_prefix(id_prefix) {
-        id_prefix[current_subdim] = i;
+        id_prefix[current_subdim] = i + 1;
     }
 
-    /**
-     * \brief Access the next dimension's accessor subscript.
-     *
-     * The resulting subscript object will contain the current prefix together with the given index.
-     * This implementation is only available to intermediate dimensions. For the last dimension, the
-     * resulting ID will be evaluated and returned.
-     *
-     * \param i The next dimension's index.
-     *
-     * \return A subscript object for the following dimensions.
-     */
     AccessorSubscript<Cell, Accessor, access_mode, current_subdim + 1> operator[](std::size_t i)
         requires(current_subdim < dimensions - 2)
     {
-        return AccessorSubscript(ac, id_prefix, i);
+        return AccessorSubscript(ac, id_prefix, i + 1);
     }
 
-    /**
-     * \brief Access the cell.
-     *
-     * This will add the given index to the prefix and use the underlying accessor to provide a
-     * reference to the cell. This is the implementation for read-only accessors as it returns a
-     * constant reference.
-     *
-     * \param i The final dimension's index.
-     *
-     * \return A constant reference to the indexed cell.
-     */
     Cell const &operator[](std::size_t i)
         requires(current_subdim == dimensions - 2 && access_mode == sycl::access::mode::read)
     {
         sycl::id<dimensions> id = id_prefix;
-        id[current_subdim + 1] = i;
+        id[current_subdim + 1] = i + 1;
         return ac[id];
     }
 
-    /**
-     * \brief Access the cell.
-     *
-     * This will add the given index to the prefix and use the underlying accessor to provide a
-     * reference to the cell. This is the implementation for accessors that may alter the grid.
-     *
-     * \param i The final dimension's index.
-     *
-     * \return A reference to the indexed cell.
-     */
     Cell &operator[](std::size_t i)
         requires(current_subdim == dimensions - 2 && access_mode != sycl::access::mode::read)
     {
         sycl::id<dimensions> id = id_prefix;
-        id[current_subdim + 1] = i;
+        id[current_subdim + 1] = i + 1;
         return ac[id];
     }
 
@@ -139,4 +46,3 @@ class AccessorSubscript {
     Accessor &ac;
     sycl::id<dimensions> id_prefix;
 };
-} // namespace stencil
--- StencilStream/Stencil.hpp
+++ StencilStream/Stencil.hpp
@@ -1,83 +1,26 @@
-/*
- * Copyright © 2020-2024 Jan-Oliver Opdenhövel, Paderborn Center for Parallel Computing, Paderborn
- * University
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
- * associated documentation files (the “Software”), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge, publish, distribute,
- * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or
- * substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
- * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 #pragma once
-#include "Helpers.hpp"
 #include <bit>
+#include <sycl/ext/intel/ac_types/ac_int.hpp>
 #include <sycl/id.hpp>
 #include <sycl/range.hpp>
 #include <variant>
 
-namespace stencil {
-
-/**
- * \brief The stencil buffer.
- *
- * The stencil buffer contains the extended Moore neighborhood of a central cell and is used by a
- * transition function to calculate the next iteration of the central cell.
- *
- * This implementation provides two ways to index the stencil: With an `ID` and a `UID`. Since `ID`
- * is signed, the column and row axes are within the range of [-radius : radius]. Therefore, (0,0)
- * points to the central cell. `UID` is unsigned and the column and row axes are within the range of
- * [0 : 2*radius + 1). Therefore, (0,0) points to the north-western corner of the stencil.
- *
- * \tparam Cell The type of cells in the stencil
- * \tparam stencil_radius The radius of the stencil, i.e. the extent of the stencil in each
- * direction from the central cell. \tparam TimeDependentValue The type of values provided by the
- * TDV system.
- */
-template <typename Cell, std::size_t stencil_radius, typename TimeDependentValue = std::monostate>
+template <typename Cell, std::size_t stencil_radius>
     requires std::semiregular<Cell> && (stencil_radius >= 1)
 class Stencil {
   public:
-    /// \brief The diameter (aka width and height) of the stencil buffer.
     static constexpr std::size_t diameter = 2 * stencil_radius + 1;
     static_assert(diameter <= std::numeric_limits<int>::max());
 
-    /**
-     * \brief Create a new stencil with an uninitialized buffer.
-     *
-     * \param id The position of the central cell in the global grid.
-     * \param grid_range The range of the underlying grid.
-     * \param iteration The present iteration index of the cells in the stencil.
-     * \param subiteration The present sub-iteration index of the cells in the stencil.
-     * \param tdv The time-dependent value for this iteration.
-     */
     Stencil(sycl::id<2> id, sycl::range<2> grid_range, std::size_t iteration,
-            std::size_t subiteration, TimeDependentValue tdv)
+            std::size_t subiteration)
         : id(id), iteration(iteration), subiteration(subiteration), grid_range(grid_range),
-          time_dependent_value(tdv), internal() {}
+          internal() {}
 
-    /**
-     * \brief Create a new stencil with the given contents.
-     *
-     * \param id The position of the central cell in the global grid.
-     * \param grid_range The range of the underlying grid.
-     * \param iteration The present iteration index of the cells in the stencil.
-     * \param subiteration The present sub-iteration index of the cells in the stencil.
-     * \param tdv The time-dependent value for this iteration.
-     * \param raw An array of cells, which is copied into the stencil object.
-     */
     Stencil(sycl::id<2> id, sycl::range<2> grid_range, std::size_t iteration,
-            std::size_t subiteration, TimeDependentValue tdv, Cell raw[diameter][diameter])
+            std::size_t subiteration, Cell raw[diameter][diameter])
         : id(id), iteration(iteration), subiteration(subiteration), grid_range(grid_range),
-          time_dependent_value(tdv), internal() {
+          internal() {
 #pragma unroll
         for (std::size_t r = 0; r < diameter; r++) {
 #pragma unroll
@@ -109,38 +52,18 @@ class Stencil {
         return StencilSubscript<index_t>(*this, r);
     }
 
-    /**
-     * \brief Access a cell in the stencil.
-     *
-     * Since the indices in `id` are unsigned, the origin of this index operator is the
-     * north-western corner.
-     */
     Cell const &operator[](sycl::id<2> id) const { return internal[id[0]][id[1]]; }
 
-    /**
-     * \brief Access a cell in the stencil.
-     *
-     * Since the indices in `id` are unsigned, the origin of this index operator is the
-     * north-western corner.
-     */
     Cell &operator[](sycl::id<2> id) { return internal[id[0]][id[1]]; }
 
-    /// \brief The position of the central cell in the global grid.
     const sycl::id<2> id;
 
-    /// \brief The present iteration index of the cells in the stencil.
     const std::size_t iteration;
 
-    /// \brief The present sub-iteration index of the cells in the stencil.
     const std::size_t subiteration;
 
-    /// \brief The range of the underlying grid.
     const sycl::range<2> grid_range;
-
-    /// \brief The time-dependent value for this iteration.
-    const TimeDependentValue time_dependent_value;
 
   private:
     Cell internal[diameter][diameter];
 };
-} // namespace stencil
