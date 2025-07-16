103307d629a322bf0c7d557173654d08b5dce0ce
Jan-Oliver Opdenhövel <joo@mail.uni-paderborn.de>
2025-07-16 15:46:40 +0200

    Switching to the MAX BSP (with direct link support)

--- examples/fdtd/scripts/run_benchmark.sh
+++ examples/fdtd/scripts/run_benchmark.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH -A hpc-lco-kenter -p fpga --constraint=bittware_520n_20.4.0_hpc
+#SBATCH -A hpc-lco-kenter -p fpga --constraint=bittware_520n_20.4.0_max
 #SBATCH --mem 128G -t 04:00:00
 #SBATCH --mail-type=ALL --mail-user=joo@mail.upb.de -J fdtd
 
