e6f0e17f6d600ee52674f41fa119bc58df1c234d
Jan-Oliver Opdenhövel <joo@mail.uni-paderborn.de>
2025-08-12 09:29:38 +0200

    Reusing the internal synthesis script for jacobi

--- examples/jacobi/scripts/synthesize.sh
+++ examples/jacobi/scripts/synthesize.sh
@@ -2,5 +2,6 @@
 #SBATCH -A hpc-lco-kenter -p normal -q fpgasynthesis --array=0-15 -c 8 --mem 120G -t 1-00:00:00
 
 task_id=${SLURM_ARRAY_TASK_ID}
-targets=("Jacobi1General_mono" "Jacobi1General_tiling" "Jacobi2Constant_mono" "Jacobi2Constant_tiling" "Jacobi3Constant_mono" "Jacobi3Constant_tiling" "Jacobi4Constant_mono" "Jacobi4Constant_tiling" "Jacobi5Constant_mono" "Jacobi5Constant_tiling" "Jacobi4General_mono" "Jacobi4General_tiling" "Jacobi5General_mono" "Jacobi5General_tiling" "Jacobi9General_mono" "Jacobi9General_tiling")
-make ${targets[$task_id]}
+variant=$1
+targets=("Jacobi1General" "Jacobi2Constant" "Jacobi3Constant" "Jacobi4Constant" "Jacobi5Constant" "Jacobi4General" "Jacobi5General" "Jacobi9General")
+make "${targets[$task_id]}_$variant"
