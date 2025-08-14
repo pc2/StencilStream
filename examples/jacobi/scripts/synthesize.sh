62049b39c3e80a6b73e371c97a26964d416e6872
Jan-Oliver Opdenhövel <joo@mail.uni-paderborn.de>
2025-08-14 14:43:27 +0200

    Building all jacobi variants again

--- examples/jacobi/scripts/synthesize.sh
+++ examples/jacobi/scripts/synthesize.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-#SBATCH -A hpc-lco-kenter -p normal -q fpgasynthesis --array=0-4 -c 8 --mem 120G -t 1-00:00:00
+#SBATCH -A hpc-lco-kenter -p normal -q fpgasynthesis --array=0-7 -c 8 --mem 120G -t 1-00:00:00
 
 task_id=${SLURM_ARRAY_TASK_ID}
 variant=$1
-#targets=("Jacobi1General" "Jacobi2Constant" "Jacobi3Constant" "Jacobi4Constant" "Jacobi5Constant" "Jacobi4General" "Jacobi5General" "Jacobi9General")
-targets=("Jacobi1General" "Jacobi2Constant" "Jacobi3Constant" "Jacobi4Constant" "Jacobi9General")
+targets=("Jacobi1General" "Jacobi2Constant" "Jacobi3Constant" "Jacobi4Constant" "Jacobi5Constant" "Jacobi4General" "Jacobi5General" "Jacobi9General")
 
 mkdir -p build
 cd build
