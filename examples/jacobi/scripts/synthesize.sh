c0508c88068e0ddab24582380cbd49681f582d72
Jan-Oliver Opdenhövel <joo@mail.uni-paderborn.de>
2025-08-16 13:03:20 +0200

    Increasing Jacobi's task array to 0-8

--- examples/jacobi/scripts/synthesize.sh
+++ examples/jacobi/scripts/synthesize.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH -A hpc-lco-kenter -p normal -q fpgasynthesis --array=0-7 -c 8 --mem 120G -t 1-00:00:00
+#SBATCH -A hpc-lco-kenter -p normal -q fpgasynthesis --array=0-8 -c 8 --mem 120G -t 1-00:00:00
 
 task_id=${SLURM_ARRAY_TASK_ID}
 variant=$1
